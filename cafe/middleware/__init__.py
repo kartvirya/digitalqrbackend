c34232e9484ae8ccaa71f59cce3ba22c594fb695
kartvirya <bikashtail619@gmail.com>
2026-05-02 11:00:18 +0545

    fix(csrf): add CsrfExemptMiddleware to middleware package __init__

--- cafe/middleware/__init__.py
+++ cafe/middleware/__init__.py
@@ -1,12 +1,23 @@
 # Middleware package for cafe application
+from django.utils.deprecation import MiddlewareMixin
+
 from .rate_limit import (
     RateLimitMiddleware,
     SecurityHeadersMiddleware,
     TenantSubscriptionGuardMiddleware,
 )
 
+
+class CsrfExemptMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        if request.path.startswith('/api/'):
+            setattr(request, '_dont_enforce_csrf_checks', True)
+        return None
+
+
 __all__ = [
+    'CsrfExemptMiddleware',
     'RateLimitMiddleware',
-    'SecurityHeadersMiddleware', 
+    'SecurityHeadersMiddleware',
     'TenantSubscriptionGuardMiddleware',
 ]
